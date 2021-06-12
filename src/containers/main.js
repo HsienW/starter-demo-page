84843f0bec666bb733231c9bda1dae1956424b5a
Wallace H Wu <wallace_h_wu@wistron.com>
2021-06-12 10:40:48 +0800

    test

--- src/containers/main.js
+++ src/containers/main.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Main = () => {
     return (
         <div>
-            <h2>React-Working-Starter is working!</h2>
+            <h2>React-Working-Starter is working! test</h2>
         </div>
     );
 };
