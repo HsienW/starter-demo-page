30f65c661a74aee6d3b2d79802e8412efac10856
Wallace H Wu <wallace_h_wu@wistron.com>
2021-06-14 17:10:51 +0800

    update main & index

--- src/containers/main.js
+++ src/containers/main.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Main = () => {
     return (
         <div data-testid='mainBlock'>
-            <h2>React-Working-Starter is working!</h2>
+            <h2>Starter Demo Page is working!</h2>
         </div>
     );
 };
