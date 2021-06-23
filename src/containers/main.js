1b8c6c8016662468433d22bc9f35a8b2713750b6
Wallace H Wu <wallace_h_wu@wistron.com>
2021-06-23 22:07:33 +0800

    super main update test

--- src/containers/main.js
+++ src/containers/main.js
@@ -4,6 +4,7 @@ export const Main = () => {
     return (
         <div data-testid='mainBlock'>
             <h2>Starter Demo Page is working!</h2>
+            <h2>Super update test</h2>
         </div>
     );
 };
